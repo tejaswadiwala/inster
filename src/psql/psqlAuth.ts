3fb3e7e525ddcf225b919f8b9adae6c644163ee7
Tejas Wadiwala <tejaswadiwala@gmail.com>
2023-08-29 12:37:35 -0400

    feat(INSTER): db url

--- src/psql/psqlAuth.ts
+++ src/psql/psqlAuth.ts
@@ -1,21 +1,11 @@
 import { Pool } from 'pg'
-import {
-  PG_DATABASE_USER,
-  PG_DATABASE_HOST,
-  PG_DATABASE_NAME,
-  PG_DATABASE_PASSWORD,
-  PG_DATABASE_PORT,
-} from '../config'
+import { DATABASE_URL } from '../config'
 
 const pool = new Pool({
-  user: PG_DATABASE_USER,
-  host: PG_DATABASE_HOST,
-  database: PG_DATABASE_NAME,
-  password: PG_DATABASE_PASSWORD,
-  port: parseInt(PG_DATABASE_PORT as string),
+  connectionString: DATABASE_URL,
   ssl: {
     rejectUnauthorized: false, // Temporary solution to SSL verification issue
-  }
+  },
 })
 
 export default pool
